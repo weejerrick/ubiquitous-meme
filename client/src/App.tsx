36dc55cddbfc6e9e970d8f3af12370ebf85d1971
Wee Jerrick <weejerrick@gmail.com>
2024-01-23 04:15:22 +0000

    Add upload and submit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,33 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import './App.css'
 
-const maxLength = 4096;
+const MAX_LENGTH = 4096;
 function App() {
-
   const [count, setCount] = useState(0);
 
-  const handleTextChange = (e) => {
+  const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setCount(e.target.value?.length);
   };
 
   return (
-    <>
+    <div>
       <div style={{ marginBottom: 20 }}>
         <h1 style={{ margin: 0 }}>Malicious Code Detector 🚨</h1>
         <sub>Input your instructions here</sub>
       </div>
-      <textarea maxLength={10} style={{ height: 200, width: 500 }} onChange={
+      <textarea maxLength={MAX_LENGTH} style={{ height: 200, width: 500 }} onChange={
         handleTextChange
       } />
-      <div id="the-count">
-        <span>{count}</span>
-        <span>/ {maxLength}</span>
+      <div style={{ marginTop: 5, marginBottom: 20 }}>
+        <span>{count}/{MAX_LENGTH}</span>
       </div>
-    </>
+      <div style={{ marginBottom: 20 }}>
+        <input type="file" id="avatar" name="avatar"
+          accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        />
+      </div>
+      <button>Submit</button>
+    </div>
   )
 }
 
