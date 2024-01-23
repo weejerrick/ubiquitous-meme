d8564ff84341e92a8aa24b5afa5b381be9fa69d6
MiaoXight119 <157443203+MiaoXight119@users.noreply.github.com>
2024-01-23 04:18:53 +0000

    setup express

--- app/app.js
+++ app/app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
