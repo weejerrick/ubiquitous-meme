8ac3dbabaf0e8ee8a28c7ccf7592321d7257c7c9
John Kuan <founders2008@hotmail.com>
2024-01-23 04:01:35 +0000

    Add counter and maxLength to textArea

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,28 @@
+import { useState } from 'react'
 import './App.css'
 
+const maxLength = 4096;
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handleTextChange = (e) => {
+    setCount(e.target.value?.length);
+  };
 
   return (
     <>
       <div style={{ marginBottom: 20 }}>
-      <h1 style={{margin: 0}}>Malicious Code Detector 🚨</h1>
-      <sub>Input your instructions here</sub>
+        <h1 style={{ margin: 0 }}>Malicious Code Detector 🚨</h1>
+        <sub>Input your instructions here</sub>
       </div>
-      <textarea style={{ height: 200, width: 500 }} />
+      <textarea maxLength={10} style={{ height: 200, width: 500 }} onChange={
+        handleTextChange
+      } />
+      <div id="the-count">
+        <span>{count}</span>
+        <span>/ {maxLength}</span>
+      </div>
     </>
   )
 }
