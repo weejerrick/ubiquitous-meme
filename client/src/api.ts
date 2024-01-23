d592f56a35eb4f2e9e4ca465994823e720325bff
Wee Jerrick <weejerrick@gmail.com>
2024-01-23 05:49:41 +0000

    add type to api

--- client/src/api.ts
+++ client/src/api.ts
@@ -3,6 +3,9 @@ async function postData(data = {}) {
         method: "POST",
         cache: "no-cache",
         credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+          },
         body: JSON.stringify(data)
     });
     return response.json();
